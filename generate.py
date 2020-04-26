6ce1545bfb59fc6bcaddd608e03bd0f20b9b128f
Paul Brabban <paul.brabban@gmail.com>
2020-04-26 17:25:25 +0100

    try generating cv from yaml

---
+++ generate.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+if __name__ == "__main__":
+    with open('./brabban.yml') as fh:
+        data = yaml.safe_load(fh)
+
+    env = Environment(
+        loader = FileSystemLoader('.'),
+        trim_blocks=True,
+        lstrip_blocks=True,
+        variable_start_string='{~',
+        variable_end_string='~}')
+    
+    template = env.get_template('brabban_cv.jinja2')
+
+    with open('brabban_cv.out.tex', 'w') as out_fh:
+        out_fh.write(template.render(data))
